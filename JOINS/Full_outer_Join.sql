a8381097826cc4c58e8c7b802eaf914eeb9ce13e
ASHAKA SEGUN DEYON <75631815+Ashaka1@users.noreply.github.com>
2024-01-19 11:00:48 +0000

    Update Full_outer_Join.sql

--- JOINS/Full_outer_Join.sql
+++ JOINS/Full_outer_Join.sql
@@ -1,5 +1,17 @@
 -- FULL -OUTER JOINS 
 
+--1 Explanation
+
+SELECT * FROM Registrations FULL OUTER JOINS Logins
+ON Registrations.name=Logins.name
+
+
+--2 Explanation
+SELECT * FROM Registrations FULL OUTER JOINS Logins
+ON Registrations.name=Logins.name
+WHERE Registrations.reg_id IS null OR
+Logins.log_id is null
+
 SELECT payment_id,payment.customer_id,first_name 
 
 FROM payment 
