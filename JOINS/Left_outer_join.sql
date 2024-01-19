6bb22444f4627bf48764a9d9e02f5bbccd50c615
ASHAKA SEGUN DEYON <75631815+Ashaka1@users.noreply.github.com>
2024-01-19 09:36:59 +0000

    Add files via upload

---
+++ JOINS/Left_outer_join.sql
@@ -0,0 +1,27 @@
+-- LEFT OUTER JOINS 
+
+  
+
+/* 
+
+SELECT columns 
+
+FROM table1 
+
+LEFT JOIN table2 ON table1.column_name = table2.column_name; 
+
+  
+
+  
+
+*/ 
+
+SELECT payment_id,payment.customer_id,first_name 
+
+FROM payment 
+
+LEFT OUTER JOIN customer 
+
+ON payment.customer_id=customer.customer_id; 
+
+  
