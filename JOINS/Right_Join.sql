6ee5f2f47e7f4f4cf7881f442ef03829b170700e
ASHAKA SEGUN DEYON <75631815+Ashaka1@users.noreply.github.com>
2024-01-19 11:15:22 +0000

    Update Right_Join.sql

--- JOINS/Right_Join.sql
+++ JOINS/Right_Join.sql
@@ -2,3 +2,17 @@ SELECT * FROM TableA
 RIGHT OUTER JOIN TableB
 ON TableA.col_match=TableB.col_match
 WHERE TableA.id IS null
+
+
+
+/*
+
+SELECT payment_id,payment.customer_id,first_name 
+
+FROM payment 
+
+LEFT OUTER JOIN customer 
+
+ON payment.customer_id=customer.customer_id; 
+
+*/
